dfb3d92b8b8b13d876eaab1ab4031fc7d3c4fc28
WeedMatch <121552068+weedmatch@users.noreply.github.com>
2026-07-16 18:34:03 +0200

    Add carousel functionality with dot navigation

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const slides = document.querySelectorAll(".carousel-slide");
+const dots = document.querySelectorAll(".carousel-dot");
 
+dots.forEach((dot, index) => {
+
+    dot.addEventListener("click", () => {
+
+        slides.forEach(slide => {
+            slide.classList.remove("active");
+        });
+
+        dots.forEach(dot => {
+            dot.classList.remove("active");
+        });
+
+
+        slides[index].classList.add("active");
+        dots[index].classList.add("active");
+
+    });
+
+});
